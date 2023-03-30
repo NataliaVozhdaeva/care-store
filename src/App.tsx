69c0daea0156cb6f113268271ec3b0a963ee7eb9
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-03-30 17:16:03 +0200

    feat: add navigation panel

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <Navigation />
-      <Header />
+      {/* <Header /> */}
       <Routes>
         <Route path='/' element={<Catalog />} />
         <Route path='/cart' element={<Cart />} />
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,40 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function Navigation() {
   return (
-    <div>
-      <div className='flex'>
-        <p className='main-text'>г. Кокчетав, ул. Ж. Ташенова 129Б</p>
-        <p className='additional-text'>(Рынок Восточный)</p>
+    <div className='top-container'>
+      <div className='container wrapper'>
+        <div className='container group-container'>
+          <Link to='https://goo.gl/maps/gxrzYrrxVa87p8j26' className=' element-container location' target={'_blank'}>
+            <p className='main-text'>г. Кокчетав, ул. Ж. Ташенова 129Б</p>
+            <p className='additional-text'>(Рынок Восточный)</p>
+          </Link>
+          <Link to='mailto:opt.sultan@mail.ru' className=' element-container email'>
+            <p className='main-text'>opt.sultan@mail.ru</p>
+            <p className='additional-text'>На связи в любое время</p>
+          </Link>
+        </div>
+        <nav className='container'>
+          <ul className='list container nav-container'>
+            <li className='nav-item'>
+              <Link to='#'>О компании</Link>{' '}
+            </li>
+            <li className='nav-item'>
+              <Link to='#'>Доставка и оплата</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='#'>Возврат</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='#'>Контакты</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <div className='flex'>
-        <p className='main-text'>opt.sultan@mail.ru</p>
-        <p className='additional-text'>На связи в любое время</p>
-      </div>
-      <nav>
-        <ul className='flex'>
-          <li className='nav-item'>
-            <a href='№' className='nav-link'>
-              О компании
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a href='№' className='nav-link'>
-              Доставка и оплата
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a href='№' className='nav-link'>
-              Возврат
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a href='№' className='nav-link'>
-              Контакты
-            </a>
-          </li>
-        </ul>
-      </nav>
     </div>
   );
 }
