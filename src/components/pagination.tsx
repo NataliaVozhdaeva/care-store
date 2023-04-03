2ffa3cbf8c3c4c0207b0d1261f5ad50d32a7b6f3
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-04-03 03:59:44 +0200

    Updates

--- src/components/pagination.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react';
-/* 
-import usePagination, { DOTS } from './usePagination'; */
-
-const TablePagination = () => {
-  return <div className='custom-table__pagination'></div>;
-};
-
-export default TablePagination;
--- src/pages/goodCart.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
-export function GoodCart() {
-  const params = useParams<'id'>();
-
-  return <div className='card-container'>Card {params.id}</div>;
-}
